71955e0baf3b0bac908291bbdad7af3a9c264364
anhbang369 <damanhbangthpt@gmail.com>
2024-03-27 09:45:28 +0700

    first commit

---
+++ src/main/java/com/example/manageeducation/ManageEducationApplication.java
@@ -0,0 +1,13 @@
+package com.example.manageeducation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageEducationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageEducationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/manageeducation/ManageEducationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.manageeducation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageEducationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
