73ba150ca4c5f301932c1af5e51b7e2a75ace3b0
anhbang369 <damanhbangthpt@gmail.com>
2024-04-03 16:36:29 +0700

    fix create unit, implement update unit

--- src/main/java/com/example/manageeducation/controller/SyllabusUnitController.java
+++ src/main/java/com/example/manageeducation/controller/SyllabusUnitController.java
@@ -1,15 +1,15 @@
 package com.example.manageeducation.controller;
 
+import com.example.manageeducation.dto.request.SyllabusUnitAddRequest;
 import com.example.manageeducation.entity.SyllabusLevel;
 import com.example.manageeducation.entity.SyllabusUnit;
 import com.example.manageeducation.service.SyllabusUnitService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/auth/syllabus")
@@ -18,9 +18,13 @@ public class SyllabusUnitController {
     @Autowired
     SyllabusUnitService syllabusUnitService;
 
-    @PostMapping("/syllabus-unit")
-    @PreAuthorize("hasRole('USER')")
-    public ResponseEntity<?> createSyllabusUnit(@RequestBody SyllabusUnit dto) {
-        return ResponseEntity.ok(syllabusUnitService.createSyllabusUnit(dto));
+    @PostMapping("/{syllabus-id}/syllabus-day/{syllabus-day-id}/syllabus-unit")
+    public ResponseEntity<?> createSyllabusUnit(@PathVariable("syllabus-id") UUID syllabusId, @PathVariable("syllabus-day-id") UUID dayId, @RequestBody SyllabusUnitAddRequest dto) {
+        return ResponseEntity.ok(syllabusUnitService.createSyllabusUnit(syllabusId,dayId,dto));
+    }
+
+    @PutMapping("/syllabus-day/syllabus-unit/{syllabus-unit-id}")
+    public ResponseEntity<?> createSyllabusUnit(@PathVariable("syllabus-unit-id") UUID unitId, @RequestBody SyllabusUnitAddRequest dto) {
+        return ResponseEntity.ok(syllabusUnitService.updateSyllabusUnit(unitId,dto));
     }
 }
