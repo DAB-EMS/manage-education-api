3b7aa15ff6a3c0bfe3bf21f97a09669183c140ae
anhbang369 <damanhbangthpt@gmail.com>
2024-04-04 19:06:43 +0700

    view training program

--- src/main/java/com/example/manageeducation/service/TrainingProgramService.java
+++ src/main/java/com/example/manageeducation/service/TrainingProgramService.java
@@ -1,9 +1,13 @@
 package com.example.manageeducation.service;
 
 import com.example.manageeducation.dto.request.TrainingProgramRequest;
+import com.example.manageeducation.dto.response.TrainingProgramResponse;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public interface TrainingProgramService {
     String createTrainingProgram(String customerId, TrainingProgramRequest dto);
+    TrainingProgramResponse viewTrainingProgram(UUID id);
 }
