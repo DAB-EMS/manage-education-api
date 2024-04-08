6c3ee7ff71a2b12eb79c7afcb36ba64c3811a67a
anhbang369 <damanhbangthpt@gmail.com>
2024-04-08 09:47:51 +0700

    add more property for customer, implement read customer but not yet test

---
+++ src/main/java/com/example/manageeducation/dto/response/CustomerResponse.java
@@ -0,0 +1,25 @@
+package com.example.manageeducation.dto.response;
+
+import com.example.manageeducation.entity.Role;
+import com.example.manageeducation.enums.Gender;
+import lombok.*;
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class CustomerResponse {
+    private String id;
+
+    @NonNull
+    private String email;
+    private String fullName;
+
+    private Date birthday;
+    private Gender gender;
+    private String level;
+
+    private Role role;
+    private String status;
+}
--- src/main/java/com/example/manageeducation/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/example/manageeducation/service/impl/CustomerServiceImpl.java
@@ -1,19 +1,26 @@
 package com.example.manageeducation.service.impl;
 
+import com.example.manageeducation.dto.response.CustomerResponse;
 import com.example.manageeducation.entity.Customer;
 import com.example.manageeducation.exception.BadRequestException;
 import com.example.manageeducation.repository.CustomerRepository;
 import com.example.manageeducation.service.CustomerService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class CustomerServiceImpl implements CustomerService {
 
     @Autowired
     CustomerRepository customerRepository;
+
+    @Autowired
+    ModelMapper modelMapper;
     @Override
     public Customer GetCustomerByEmail(String email) {
         try{
@@ -26,5 +33,13 @@ public class CustomerServiceImpl implements CustomerService {
         }catch (Exception e){
             return null;
         }
+    }
+
+    @Override
+    public List<CustomerResponse> userList(String search) {
+        List<Customer> customerList = customerRepository.findAll();
+        return customerList.stream()
+                .map(customer -> modelMapper.map(customer, CustomerResponse.class))
+                .collect(Collectors.toList());
     }
 }
