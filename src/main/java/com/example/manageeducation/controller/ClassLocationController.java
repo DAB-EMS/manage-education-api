a263ec06dcec37434a9de1da43d7ee2aeacfd5e6
anhbang369 <damanhbangthpt@gmail.com>
2024-04-28 11:28:22 +0700

    get all class location

---
+++ src/main/java/com/example/manageeducation/controller/ClassLocationController.java
@@ -0,0 +1,21 @@
+package com.example.manageeducation.controller;
+
+import com.example.manageeducation.service.ClassLocationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class ClassLocationController {
+
+    @Autowired
+    ClassLocationService classLocationService;
+
+    @GetMapping("/class-locations")
+    public ResponseEntity<?> classLocation() {
+        return ResponseEntity.ok(classLocationService.classLocationResponses());
+    }
+}
