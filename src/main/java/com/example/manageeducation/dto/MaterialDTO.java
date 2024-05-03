de45e5d273980b546dc6be2b59ef3976732f41ac
anhbang369 <damanhbangthpt@gmail.com>
2024-05-03 12:21:48 +0700

    config show name in material

---
+++ src/main/java/com/example/manageeducation/dto/MaterialDTO.java
@@ -0,0 +1,22 @@
+package com.example.manageeducation.dto;
+
+import com.example.manageeducation.enums.MaterialStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.Date;
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class MaterialDTO {
+    private UUID id;
+    private String name;
+    private String url;
+    private String createdBy;
+    private Date createdDate;
+    private MaterialStatus materialStatus;
+}
