f012c43019a28fc5b3a98fc01b6b089c91adad14
anhbang369 <damanhbangthpt@gmail.com>
2024-05-05 11:47:12 +0700

    add PreAuthorize by DeliveryTypeController, FormatTypeController, FsuController and MaterialController

--- src/main/java/com/example/manageeducation/controller/FormatTypeController.java
+++ src/main/java/com/example/manageeducation/controller/FormatTypeController.java
@@ -4,6 +4,7 @@ import com.example.manageeducation.entity.FormatType;
 import com.example.manageeducation.service.FormatTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ public class FormatTypeController {
     @Autowired
     FormatTypeService formatTypeService;
 
+    @PreAuthorize("hasAuthority('VIEW_SYLLABUS')")
     @GetMapping("/format-types")
     public ResponseEntity<?> attendLevels() {
         return ResponseEntity.ok(formatTypeService.formatTypeResponse());
