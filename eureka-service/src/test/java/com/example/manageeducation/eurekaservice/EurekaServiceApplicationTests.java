7f8a449975cb80362cc8a60c4686f26b8f5118d6
anhbang369 <damanhbangthpt@gmail.com>
2024-05-22 19:54:51 +0700

    add eureka-service for microservice

---
+++ eureka-service/src/test/java/com/example/manageeducation/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.manageeducation.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
