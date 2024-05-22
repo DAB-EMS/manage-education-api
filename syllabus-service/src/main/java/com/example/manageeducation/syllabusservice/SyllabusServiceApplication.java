282f3da0bd0ea0bd4d3893f4633e21ddfac88826
anhbang369 <damanhbangthpt@gmail.com>
2024-05-22 20:27:12 +0700

    add syllabus-service for microservice

---
+++ syllabus-service/src/main/java/com/example/manageeducation/syllabusservice/SyllabusServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.manageeducation.syllabusservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyllabusServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyllabusServiceApplication.class, args);
+	}
+
+}
