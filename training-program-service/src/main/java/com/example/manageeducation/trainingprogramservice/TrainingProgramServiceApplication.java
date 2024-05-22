eec9e8de363415ad19e0ea70f2d515dd8c2eaefa
anhbang369 <damanhbangthpt@gmail.com>
2024-05-22 22:18:40 +0700

    add training-program-service for microservice

---
+++ training-program-service/src/main/java/com/example/manageeducation/trainingprogramservice/TrainingProgramServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.manageeducation.trainingprogramservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingProgramServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingProgramServiceApplication.class, args);
+	}
+
+}
